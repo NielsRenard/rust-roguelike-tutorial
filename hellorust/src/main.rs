4ebdf527f83c28ca46d452396db5bbd9c60ca5c2
niels <place@holder.com>
2019-12-30 13:46:20 +0100

    TileTypes added, random walls

--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -15,6 +15,8 @@ impl GameState for State {
 
 	player_input(self, ctx);
 	self.run_systems();
+	let map = self.ecs.fetch::<Vec<TileType>>();
+	draw_map(&map, ctx);
 
 	let positions = self.ecs.read_storage::<Position>();
 	let renderables = self.ecs.read_storage::<Renderable>();
@@ -40,6 +42,9 @@ fn main() {
     let mut gs = State {
 	ecs: World::new()
     };
+    // add a map to the world
+    gs.ecs.insert(new_map());
+    
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
@@ -133,3 +138,62 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+#[derive (PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor
+}
+
+pub fn xy_idx(x: i32, y: i32)-> usize {
+    return (y as usize * 80) + x as usize;
+}
+
+fn new_map() -> (Vec<TileType>) {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    for x in 0..80 {
+	map[xy_idx(x, 0)] = TileType::Wall;
+	map[xy_idx(x, 49)] = TileType::Wall;	
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+    map    
+}
+
+
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
