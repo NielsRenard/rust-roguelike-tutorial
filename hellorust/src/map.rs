37e0c1e4dee43f9cfc2a834b6be2933e8d46889f
Niels Renard <code.i.pushed@gmail.com>
2020-01-05 00:21:26 +0900

    Only draw tiles that are visible

--- hellorust/src/map.rs
+++ hellorust/src/map.rs
@@ -1,5 +1,5 @@
 extern crate rltk;
-use super::Rect;
+use super::{Rect, Viewshed, Player};
 use rltk::{Console, RandomNumberGenerator, Rltk, RGB, Algorithm2D, BaseMap, Point};
 use std::cmp::{max, min};
 extern crate specs;
@@ -136,37 +136,46 @@ impl BaseMap for Map {
 }
 
 pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let mut players = ecs.write_storage::<Player>();
     let map = ecs.fetch::<Map>();
-    let mut y = 0;
-    let mut x = 0;
-    for tile in map.tiles.iter() {
-        // Render a tile depending upon the tile type
-        match tile {
-            TileType::Floor => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.5, 0.5, 0.5),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('.'),
-                );
-            }
-            TileType::Wall => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.0, 1.0, 0.0),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('#'),
-                );
-            }
-        }
 
-        // Move the coordinates
-        x += 1;
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
+
+    for (_player, viewshed) in (&mut players, &mut viewsheds).join() {
+	let mut y = 0;
+	let mut x = 0;
+	for tile in map.tiles.iter() {
+            // Render a tile depending upon the tile type
+            let pt = Point::new(x,y);
+	    if viewshed.visible_tiles.contains(&pt) {
+		match tile {
+		    TileType::Floor => {
+			ctx.set(
+			    x,
+			    y,
+			    RGB::from_f32(0.5, 0.5, 0.5),
+			    RGB::from_f32(0., 0., 0.),
+			    rltk::to_cp437('.'),
+			);
+		    }
+		    TileType::Wall => {
+			ctx.set(
+			    x,
+			    y,
+			    RGB::from_f32(0.0, 1.0, 0.0),
+			    RGB::from_f32(0., 0., 0.),
+			    rltk::to_cp437('#'),
+			);
+		    }
+		}
+	    }
+	    
+            // Move the coordinates
+            x += 1;
+            if x > 79 {
+		x = 0;
+		y += 1;
+            }
+	}
     }
 }
