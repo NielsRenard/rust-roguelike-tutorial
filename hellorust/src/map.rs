b02931a3acb72e02ad0129a5acdd80834a402d56
Niels Renard <code.i.pushed@gmail.com>
2020-01-05 02:44:11 +0900

    Don't use abbreviated float literals

--- hellorust/src/map.rs
+++ hellorust/src/map.rs
@@ -159,7 +159,7 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
                 }
                 TileType::Wall => {
                     glyph = rltk::to_cp437('#');
-                    fg = RGB::from_f32(0., 1.0, 0.);
+                    fg = RGB::from_f32(0.0, 1.0, 0.0);
                 }
             }
             if !map.visible_tiles[idx] {
