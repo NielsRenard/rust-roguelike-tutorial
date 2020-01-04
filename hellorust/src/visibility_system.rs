21aa27726f391e29e166ec15ff74f6fd400244b4
Niels Renard <code.i.pushed@gmail.com>
2020-01-05 00:00:03 +0900

    Expand VisibilitySystem to store visible tiles

--- hellorust/src/visibility_system.rs
+++ hellorust/src/visibility_system.rs
@@ -1,13 +1,24 @@
 extern crate specs;
-use super::{Position, Viewshed};
+use super::{Position, Viewshed, Map};
 use specs::prelude::*;
+extern crate rltk;
+use rltk::{field_of_view, Point};
 
 pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
-    type SystemData = (WriteStorage<'a, Viewshed>, WriteStorage<'a, Position>);
+    type SystemData = (ReadExpect<'a, Map>, WriteStorage<'a, Viewshed>, WriteStorage<'a, Position>);
 
-    fn run(&mut self, (mut viewshed, pos): Self::SystemData) {
-        for (viewshed, pos) in (&mut viewshed, &pos).join() {}
+    fn run(&mut self, data: Self::SystemData) {
+	let (map, mut viewshed, pos) = data;
+	
+        for (viewshed, pos) in (&mut viewshed, &pos).join() {
+	    viewshed.visible_tiles.clear();
+	    // &* is syntax to dereference, then get a reference →→→→→→→→→→→→→→→→→→→→→→→→→→→→↓
+	    viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+            viewshed.visible_tiles.retain(|p : &Point| p.x > 0 && p.x < map.width-1 && p.y > 0 && p.y < map.height-1 );
+	    // This will now run every frame (which is overkill, more on that later)
+	    // and store a list of visible tiles.	    
+	}
     }
 }
