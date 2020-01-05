325e0e1558ba5d73734a3078b1582198975a2df3
Niels Renard <code.i.pushed@gmail.com>
2020-01-05 13:41:17 +0900

    Add monsters to rooms

--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -1,5 +1,5 @@
 rltk::add_wasm_support!();
-use rltk::{Console, GameState, Rltk, RGB};
+use rltk::{Console, GameState, RandomNumberGenerator, Rltk, RGB};
 use specs::prelude::*;
 #[macro_use]
 extern crate specs_derive;
@@ -29,9 +29,12 @@ impl GameState for State {
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-
+        let map = self.ecs.fetch::<Map>();
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
     }
 }
@@ -59,6 +62,32 @@ fn main() {
     let map: Map = Map::new_map_rooms_and_corridors();
     // make sure the player doesn't get put inside wall
     let (player_x, player_y) = map.rooms[0].center();
+
+    // every room -except the first one- gets a monster
+    let mut rng = RandomNumberGenerator::new();
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        let glyph: u8;
+        let roll = rng.roll_dice(1, 2);
+        match roll {
+            1 => glyph = rltk::to_cp437('g'),
+            _ => glyph = rltk::to_cp437('o'),
+        }
+        gs.ecs
+            .create_entity()
+            .with(Position { x, y })
+            .with(Renderable {
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed {
+                visible_tiles: Vec::new(),
+                range: 8,
+                dirty: true,
+            })
+            .build();
+    }
 
     gs.ecs.insert(map);
 
