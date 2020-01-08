c2ad5d41bdc21309b068a98898b86f13ab55e242
Niels Renard <code.i.pushed@gmail.com>
2020-01-08 22:57:50 +0900

    Remove unnecessary imports

--- hellorust/src/gamelog.rs
+++ hellorust/src/gamelog.rs
@@ -1,9 +1,3 @@
-extern crate rltk;
-use rltk::{Console, Rltk, RGB};
-extern crate specs;
-use super::{CombatStats, Player};
-use specs::prelude::*;
-
 pub struct GameLog {
     pub entries: Vec<String>,
 }
