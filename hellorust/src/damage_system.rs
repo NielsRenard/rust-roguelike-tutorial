59f85ac2108e2aac48a36a0aa1b4e52cf459ca32
Niels Renard <code.i.pushed@gmail.com>
2020-01-08 23:56:15 +0900

    Monster's name is printed when slain

--- hellorust/src/damage_system.rs
+++ hellorust/src/damage_system.rs
@@ -1,5 +1,5 @@
 extern crate specs;
-use super::{CombatStats, Player, SufferDamage};
+use super::{CombatStats, Name, Player, SufferDamage};
 use crate::gamelog::GameLog;
 use specs::prelude::*;
 
@@ -26,6 +26,7 @@ pub fn delete_the_dead(ecs: &mut World) {
     // Using a scope to make the borrow checker happy
     {
         let combat_stats = ecs.read_storage::<CombatStats>();
+        let names = ecs.read_storage::<Name>();
         let players = ecs.read_storage::<Player>();
         let entities = ecs.entities();
         let mut log = ecs.write_resource::<GameLog>();
@@ -34,8 +35,21 @@ pub fn delete_the_dead(ecs: &mut World) {
             if stats.hp < 1 {
                 let player = players.get(entity);
                 match player {
-                    None => dead.push(entity),
                     Some(_) => log.entries.insert(0, String::from("You are dead")),
+                    None => {
+                        // TODO: not too stoked on this nested match expression
+                        match names.get(entity) {
+                            Some(victim_name) => log
+                                .entries
+                                .insert(0, format!("{} was slain", victim_name.name)),
+                            None => {
+                                // TODO: leaving this in for now but
+                                // entities with no name probably shouldn't happen
+                                log.entries.insert(0, format!("unknown entity was slain"))
+                            }
+                        }
+                        dead.push(entity)
+                    }
                 }
             }
         }
