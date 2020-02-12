b216f0a3a8d3be743ac9b4bdb8af1e40c190df6a
Niels Renard <code.i.pushed@gmail.com>
2020-02-12 18:59:58 +0100

    Better loot at lower levels (game now imbalanced)

--- hellorust/src/spawner.rs
+++ hellorust/src/spawner.rs
@@ -54,8 +54,12 @@ fn room_table(map_depth: i32) -> RandomTable {
         .add("Health Potion", 7)
         .add("Magic Missile Scroll", 4)
         .add("Orc", 1 + map_depth)
-        .add("Dagger", 30)
-        .add("Shield", 30)
+        .add("Dagger", 3)
+        .add("Shield", 3)
+        .add("Dagger", 3)
+        .add("Shield", 3)
+        .add("Longsword", map_depth - 1)
+        .add("Tower Shield", map_depth - 1)
 }
 
 pub fn orc(ecs: &mut World, x: i32, y: i32) {
@@ -135,6 +139,8 @@ pub fn spawn_room(ecs: &mut World, room: &Rect, map_depth: i32) {
             "Orc" => orc(ecs, x, y),
             "Dagger" => dagger(ecs, x, y),
             "Shield" => shield(ecs, x, y),
+            "Longsword" => longsword(ecs, x, y),
+            "Tower Shield" => tower_shield(ecs, x, y),
             _ => {}
         }
     }
@@ -262,6 +268,48 @@ fn shield(ecs: &mut World, x: i32, y: i32) {
             slot: EquipmentSlot::Shield,
         })
         .with(DefenseBonus { defense: 1 })
+        .marked::<SimpleMarker<SerializeMe>>()
+        .build();
+}
+
+fn longsword(ecs: &mut World, x: i32, y: i32) {
+    ecs.create_entity()
+        .with(Position { x, y })
+        .with(Renderable {
+            glyph: rltk::to_cp437('/'),
+            fg: yellow(),
+            bg: black(),
+            render_order: 2,
+        })
+        .with(Name {
+            name: "Longsword".to_string(),
+        })
+        .with(Item {})
+        .with(Equippable {
+            slot: EquipmentSlot::Melee,
+        })
+        .with(MeleePowerBonus { power: 4 })
+        .marked::<SimpleMarker<SerializeMe>>()
+        .build();
+}
+
+fn tower_shield(ecs: &mut World, x: i32, y: i32) {
+    ecs.create_entity()
+        .with(Position { x, y })
+        .with(Renderable {
+            glyph: rltk::to_cp437('('),
+            fg: yellow(),
+            bg: black(),
+            render_order: 2,
+        })
+        .with(Name {
+            name: "Tower Shield".to_string(),
+        })
+        .with(Item {})
+        .with(Equippable {
+            slot: EquipmentSlot::Shield,
+        })
+        .with(DefenseBonus { defense: 3 })
         .marked::<SimpleMarker<SerializeMe>>()
         .build();
 }
