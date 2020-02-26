2f19122e624c4e986905c4c02bed762567cdd68e
Niels Renard <code.i.pushed@gmail.com>
2020-02-26 22:55:44 +0100

    Fix bug where player took no damage from aoe (splash) damage

--- hellorust/src/map_indexing_system.rs
+++ hellorust/src/map_indexing_system.rs
@@ -1,5 +1,5 @@
 extern crate specs;
-use super::{BlocksTile, Map, Position};
+use super::{BlocksTile, Map, Player, Position};
 use specs::prelude::*;
 
 pub struct MapIndexingSystem {}
@@ -9,11 +9,12 @@ impl<'a> System<'a> for MapIndexingSystem {
         WriteExpect<'a, Map>,
         ReadStorage<'a, Position>,
         ReadStorage<'a, BlocksTile>,
+        ReadStorage<'a, Player>,
         Entities<'a>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut map, position, blockers, entities) = data;
+        let (mut map, position, blockers, player, entities) = data;
 
         map.populate_blocked();
         map.clear_content_index();
@@ -22,9 +23,16 @@ impl<'a> System<'a> for MapIndexingSystem {
             let idx = map.xy_idx(position.x, position.y);
 
             // If they block, update the blocking list
-            let _p: Option<&BlocksTile> = blockers.get(entity);
-            if let Some(_p) = _p {
+            let _b: Option<&BlocksTile> = blockers.get(entity);
+            if let Some(_p) = _b {
                 map.blocked_tiles[idx] = true;
+            }
+
+            // Push the player into the tile_content stack. fixes
+            // splash damage bug where player took no damage
+            let _p: Option<&Player> = player.get(entity);
+            if let Some(_player) = player.get(entity) {
+                map.tile_content[idx].push(entity);
             }
 
             // Push the entity to the appropriate index slot. It's a Copy
